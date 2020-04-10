983fc5ce041206b70950e916bbafce7c9d36d4fb
danangharissetiawan <mr.danangharissetiawan@gmail.com>
2020-04-11 02:08:39 +0700

    Eps 3-4 API Class View, Class Mixins, Class Generic, and permissions basic

--- src/mysite/urls.py
+++ src/mysite/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls import include
+
 from rest_framework import routers
+
 from blog.views import UserViewSet, GroupViewSet
 
 
@@ -13,4 +16,8 @@ urlpatterns = [
     path('', include('snippets.urls')),
     path('', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+]
+
+urlpatterns += [
+    path('api-auth/', include('rest_framework.urls')),
 ]
